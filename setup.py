53e159baaf83d86a1303da86337baa38dfef28ac
Pierre Fersing <pierre.fersing@bleemeo.com>
2015-05-22 11:06:31 +0200

    Added plugins for disk usage

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
     entry_points={
         'bleemeo_agent.plugins_v1': [
             'apache = bleemeo_agent.plugins.apache:Apache',
+            'disk = bleemeo_agent.plugins.disk:Disk',
             'mysql = bleemeo_agent.plugins.mysql:MySQL',
             'ntpd = bleemeo_agent.plugins.ntpd:Ntpd',
             'redis = bleemeo_agent.plugins.redis:Redis',
