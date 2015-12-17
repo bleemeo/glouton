42726d4c5239d38c6aacdfa5f1bacf987f0baacb
Pierre Fersing <pierre.fersing@bleemeo.com>
2015-12-17 12:05:05 +0100

    Don't use "." in memcached metric name

--- bleemeo_agent/collectd.py
+++ bleemeo_agent/collectd.py
@@ -601,6 +601,8 @@ class Collectd(threading.Thread):
             if not name.startswith('memcached_'):
                 name = 'memcached_' + name
 
+            name = name.replace('.', '_')
+
             item = match_dict['plugin_instance'].replace('bleemeo-', '')
             if item == 'None':
                 item = None
