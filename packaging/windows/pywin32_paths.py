39e21c29b484a17545ae1a51c020c7abf371be17
Pierre Fersing <pierre.fersing@bleemeo.com>
2017-02-23 14:02:34 +0100

    Fix issue with Windows 8 and before
    Add $INSTDIR\Python to PATH. This is required for compiled modules (like
    psutil) to find msvcrt DLLs bundled in the installer.
    This was not needed on Windows 10 or any Windows where msvcrt are
    installed system wide.

--- packaging/windows/pywin32_paths.py
+++ packaging/windows/pywin32_paths.py
@@ -4,6 +4,9 @@ sys.path.extend([
     os.path.join(pkgdir, 'Pythonwin'),
 ])
 
+# Needed to load msvcrt DLL.
+os.environ['PATH'] += (';' + os.path.join(scriptdir, 'Python'))
+
 # Preload pywintypes and pythoncom
 pwt = os.path.join(pkgdir, 'pypiwin32_system32', 'pywintypes36.dll')
 pcom = os.path.join(pkgdir, 'pypiwin32_system32', 'pythoncom36.dll')
