e9e875dd56f2587d13eb12aa374668d3fd9bd2bd
Adrien Gaillard <adrien.gaillard@bleemeo.com>
2018-06-26 15:41:34 +0200

    Small error correction

--- bleemeo_agent/influxdb.py
+++ bleemeo_agent/influxdb.py
@@ -151,8 +151,10 @@ class InfluxDBConnector(threading.Thread):
 
         if metric_point.item:
             influx_metric['tags']['item'] = metric_point.item
-        if metric_point.status_of is not None:
-            influx_metric['tags']['status'] = metric_point.status_of
+        if metric_point.status_code is not None:
+            influx_metric['tags']['status'] = (
+                bleemeo_agent.type.STATUS_NAME[metric_point.status_code]
+            )
 
         if self.core.agent_uuid is None:
             influx_metric['tags']['hostname'] = socket.getfqdn()
