7fb0950a111c153c00e06e824c2dc463fed3c525
Adrien Gaillard <adrien.gaillard@bleemeo.com>
2018-08-16 15:03:44 +0200

    Go implemantation of MemoryCollector

---
+++ agentgo/examples/memoryDisplay.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"../src/system"
+	"fmt"
+	"time"
+)
+
+func main() {
+	var virtualMemory = system.InitMemoryCollector()
+	for {
+		var memoryMetrics = virtualMemory.Gather()
+		fmt.Println("----------------------------------------------------------------------------")
+		for _, metricPoint := range memoryMetrics {
+			var unit = ""
+			switch metricPoint.Metric.Unit {
+			case 1:
+				unit = "B"
+			case 2:
+				unit = "%"
+			default:
+				unit = "unecpected unit"
+
+			}
+			fmt.Printf("%s : %f %s\n", metricPoint.Metric.Name, metricPoint.Value, unit)
+		}
+		time.Sleep(2 * 1000000000)
+	}
+}
