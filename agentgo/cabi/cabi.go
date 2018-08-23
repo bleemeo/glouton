e1c6aa0890d095e6f96a73f37e6a3cd78d4b06e2
Adrien Gaillard <adrien.gaillard@bleemeo.com>
2018-08-23 14:30:50 +0200

    Modification of cabi for accumulator compatibility

--- agentgo/cabi/cabi.go
+++ agentgo/cabi/cabi.go
@@ -8,17 +8,12 @@ struct Tag {
 
 typedef struct Tag Tag;
 
-enum Unit{
-	NoUnit = 0,
-	Bytes = 1,
-	Percent = 2,
-	BytesPerSecond = 3,
-	BitsPerSecond = 4,
-	MillissecondPerSecond = 5,
-	ReadsPerSecond = 6,
-	WritesPerSecond = 7,
-	PacketsPerSecond = 8,
-	ErrorsPerSecond = 9,
+enum Type{
+	fields = 0,
+	gauge = 1,
+	counter = 2,
+	summary = 3,
+	histogram = 4,
 };
 
 struct MetricPoint
@@ -26,8 +21,7 @@ struct MetricPoint
 	char* name;
 	Tag *tag;
 	int tag_count;
-	char* chart;
-	enum Unit unit;
+	enum Type metric_type;
 	float value;
 };
 
@@ -44,52 +38,79 @@ typedef struct MetricPointVector MetricPointVector;
 import "C"
 
 import (
-	"../system"
 	"../types"
+	"github.com/influxdata/telegraf"
+	// Needed to run this package
+	"github.com/influxdata/telegraf/plugins/inputs"
+	_ "github.com/influxdata/telegraf/plugins/inputs/mem"
 	"math/rand"
 )
 
-var collectors map[int]types.Collector = make(map[int]types.Collector)
+var inputsgroups = make(map[int]map[int]telegraf.Input)
 
 // FunctionTest export an int in C
 //export FunctionTest
 func FunctionTest() int64 { return 42 }
 
-// InitMemoryCollector initialises a memory collector and returns his ID
-//export InitMemoryCollector
-func InitMemoryCollector() int {
+// InitInputGroup initialises a metric group and returns his ID
+//export InitInputGroup
+func InitInputGroup() int {
 	var id = rand.Intn(10000)
 
-	for _, ok := collectors[id]; ok == true; {
+	for _, ok := inputsgroups[id]; ok == true; {
 		id = rand.Intn(10000)
 	}
-	collectors[id] = system.InitMemoryCollector()
-
+	inputsgroups[id] = make(map[int]telegraf.Input)
 	return id
 }
 
-// FreeCollector deletes a collector
-// exit code 0 : the collector has been removed
-// exit code 1 : the collector did not exist
-//export FreeCollector
-func FreeCollector(collectorID int) int {
-	_, ok := collectors[collectorID]
+// addInputToInputGroup add the input to the inputGroupID and return the inputID of the input in the group
+func addInputToInputGroup(inputGroupID int, input telegraf.Input) int {
+	var inputID = rand.Intn(10000)
+
+	for _, ok := inputsgroups[inputGroupID][inputID]; ok == true; {
+		inputID = rand.Intn(10000)
+	}
+	inputsgroups[inputGroupID][inputID] = input
+	return inputID
+}
+
+// MemoryInput add a memory input to the inputgroupID and return the input ID in the group
+//export MemoryInput
+func MemoryInput(inputGroupID int) int {
+	return addInputToInputGroup(inputGroupID, inputs.Inputs["mem"]())
+}
+
+// FreeInputGroup deletes a collector
+// exit code 0 : the input group has been removed
+// exit code 1 : the input group did not exist
+//export FreeInputGroup
+func FreeInputGroup(inputgroupID int) int {
+	_, ok := inputsgroups[inputgroupID]
 	if ok {
-		delete(collectors, collectorID)
+		for id := range inputsgroups[inputgroupID] {
+			delete(inputsgroups[inputgroupID], id)
+		}
+		delete(inputsgroups, inputgroupID)
 		return 0
 	}
 	return 1
 }
 
-// Gather returns associated metrics of collectorID given in parameter.
+// Gather returns associated metrics in a slice of inputgroupID given in parameter.
 //export Gather
-func Gather(collectorID int) C.MetricPointVector {
-	var collector, ok = collectors[collectorID]
+func Gather(inputgroupID int) C.MetricPointVector {
+	var inputgroup, ok = inputsgroups[inputgroupID]
 	var result C.MetricPointVector
 	if !ok {
 		return result
 	}
-	var metrics = collector.Gather()
+	accumulator := types.InitAccumulator()
+
+	for _, input := range inputgroup {
+		input.Gather(&accumulator)
+	}
+	metrics := accumulator.GetMetricPointSlice()
 	var metricPointArray = C.malloc(C.size_t(len(metrics)) * C.sizeof_MetricPoint)
 	var metricPointSlice = (*[1<<30 - 1]C.MetricPoint)(metricPointArray)
 
@@ -106,7 +127,7 @@ func Gather(collectorID int) C.MetricPointVector {
 
 func convertMetricPointInC(metricPoint types.MetricPoint) C.MetricPoint {
 	var tagCount int
-	for range metricPoint.Metric.Tag {
+	for range metricPoint.Tags {
 		tagCount++
 	}
 
@@ -114,7 +135,7 @@ func convertMetricPointInC(metricPoint types.MetricPoint) C.MetricPoint {
 	var tagsSlice = (*[1<<30 - 1]C.Tag)(tags)
 
 	var index int
-	for key, value := range metricPoint.Metric.Tag {
+	for key, value := range metricPoint.Tags {
 		tagsSlice[index] = C.Tag{
 			tag_name:  C.CString(key),
 			tag_value: C.CString(value),
@@ -122,38 +143,26 @@ func convertMetricPointInC(metricPoint types.MetricPoint) C.MetricPoint {
 		index++
 	}
 
-	var unit C.enum_Unit
-	switch metricPoint.Metric.Unit {
-	case 1:
-		unit = C.Bytes
-	case 2:
-		unit = C.Percent
-	case 3:
-		unit = C.BytesPerSecond
-	case 4:
-		unit = C.BitsPerSecond
-	case 5:
-		unit = C.MillissecondPerSecond
-	case 6:
-		unit = C.ReadsPerSecond
-	case 7:
-		unit = C.WritesPerSecond
-	case 8:
-		unit = C.PacketsPerSecond
-	case 9:
-		unit = C.ErrorsPerSecond
-	default:
-	case 0:
-		unit = C.NoUnit
+	var metricType C.enum_Type
+	switch metricPoint.Type {
+	case types.Gauge:
+		metricType = C.gauge
+	case types.Counter:
+		metricType = C.counter
+	case types.Histogram:
+		metricType = C.histogram
+	case types.Summary:
+		metricType = C.summary
+	case types.Fields:
+		metricType = C.fields
 	}
 
 	var result C.MetricPoint = C.MetricPoint{
-		name:      C.CString(metricPoint.Metric.Name),
-		tag:       (*C.Tag)(tags),
-		tag_count: C.int(tagCount),
-		chart:     C.CString(metricPoint.Metric.Chart),
-		unit:      unit,
-		value:     C.float(metricPoint.Value)}
+		name:        C.CString(metricPoint.Name),
+		tag:         (*C.Tag)(tags),
+		tag_count:   C.int(tagCount),
+		metric_type: metricType,
+		value:       C.float(metricPoint.Value)}
 
 	return result
 }
--- bleemeo_agent/show_metrics.py
+++ bleemeo_agent/show_metrics.py
@@ -13,30 +13,33 @@ def wrap_function(lib, funcname, restype, argtypes):
     return func
 
 
-def functionTest():
-    lib.FunctionTest.argtypes = []
-    print("Functiontest() = %d" % lib.FunctionTest())
-
-
 class MetricPoint(Structure):
     _fields_ = [('name', c_char_p),
                 ('tag', POINTER(c_int)),
                 ('tag_count', c_int),
-                ('chart', c_char_p),
-                ('unit', c_int),
+                ('metric_type', c_int),
                 ('value', c_float)]
 
 
-class MetricVector(Structure):
+class MetricPointVector(Structure):
     _fields_ = [('metric_point', POINTER(MetricPoint)),
                 ('metric_point_count', c_int)]
 
 
-initMemoryCollector = wrap_function(lib, 'InitMemoryCollector', int, None)
-gather = wrap_function(lib, 'Gather', MetricVector, [c_int, ])
-memory_collector_id = initMemoryCollector()
+# Load function from C-lib
+init_input_group = wrap_function(lib, 'InitInputGroup', int, None)
+memory_input = wrap_function(lib, "MemoryInput", int, [c_int, ])
+gather = wrap_function(lib, 'Gather', MetricPointVector, [c_int, ])
+
+# Create an input group
+input_group_id = init_input_group()
+# Add a memory input in the input group
+memory_input_id = memory_input(input_group_id)
+
 while True:
-    metrics_vector = gather(memory_collector_id)
+    # Gather metric from input group
+    metrics_vector = gather(input_group_id)
+
     print("\n-------------------------------------------------")
     for i in range(0, metrics_vector.metric_point_count):
         metric_point = (metrics_vector.metric_point[i])
