bleemeo/glouton

commit 0431a54421234020954a1ab1623985c2510c9932

Fix thread-safe issue on checks
Both update_checks and periodic_check used the same global CHECKS which
caused some exception to be logged.

bleemeo_agent/checker.py
@@ -116,6 +116,7 @@ CHECKS_INFO = {
 
 # global variable with all checks created
 CHECKS = {}
+_CHECKS_LOCK = threading.Lock()
 
 
 def update_checks(core):
@@ -125,12 +126,13 @@ def update_checks(core):
     for key, service_info in core.services.items():
         (service_name, instance) = key
         checks_seen.add(key)
-        if key in CHECKS and CHECKS[key].service_info == service_info:
-            # check unchanged
-            continue
-        elif key in CHECKS:
-            CHECKS[key].stop()
-            del CHECKS[key]
+        with _CHECKS_LOCK:
+            if key in CHECKS and CHECKS[key].service_info == service_info:
+                # check unchanged
+                continue
+            elif key in CHECKS:
+                CHECKS[key].stop()
+                del CHECKS[key]
 
         if service_info.get('ignore_check', False):
             continue
@@ -146,7 +148,8 @@ def update_checks(core):
                 instance,
                 service_info,
             )
-            CHECKS[key] = new_check
+            with _CHECKS_LOCK:
+                CHECKS[key] = new_check
         except NotImplementedError:
             logging.debug(
                 'No check exists for service %s', service_name,
@@ -158,10 +161,11 @@ def update_checks(core):
                 exc_info=True
             )
 
-    deleted_checks = set(CHECKS.keys()) - checks_seen
-    for key in deleted_checks:
-        CHECKS[key].stop()
-        del CHECKS[key]
+    with _CHECKS_LOCK:
+        deleted_checks = set(CHECKS.keys()) - checks_seen
+        for key in deleted_checks:
+            CHECKS[key].stop()
+            del CHECKS[key]
 
 
 def periodic_check():
@@ -169,8 +173,9 @@ def periodic_check():
 
         * that all TCP socket are still openned
     """
-    for check in CHECKS.values():
-        check.check_sockets()
+    with _CHECKS_LOCK:
+        for check in CHECKS.values():
+            check.check_sockets()
 
 
 class Check:
@@ -291,6 +296,10 @@ class Check:
         """
         try_reopen = False
 
+        if self.open_sockets_job is not None:
+            # open_sockets is pending, wait for it before checking sockets
+            return
+
         sockets = {}
         for key, sock in self.tcp_sockets.items():
             if sock is not None:
@@ -317,7 +326,16 @@ class Check:
                 try_reopen = True
 
         if try_reopen:
-            self.open_sockets()
+            with self._lock:
+                if self.open_sockets_job is not None:
+                    self.core.unschedule_job(self.open_sockets_job)
+                if self._closed:
+                    return
+                self.open_sockets_job = self.core.add_scheduled_job(
+                    self.open_sockets,
+                    seconds=0,
+                    next_run_in=0,
+                )
 
     def run_check(self):
         # pylint: disable=too-many-branches

commit 390d0ca3505150076081afb49daef942d1eb367a

Fix TCP socket check
A regression introduced by commit 0431a54 caused TCP socket monitoring
(used to immediately detect service down) to fail.
In fact only the very first one was correctly handled, but subsequent
one fail due to not cleared "open_sockets_job" variable.

bleemeo_agent/checker.py
@@ -257,6 +257,9 @@ class Check:
     def open_sockets(self):
         """ Try to open all closed sockets
         """
+        with self._lock:
+            self.open_sockets_job = None
+
         if self.check_info.get('disable_persistent_socket'):
             return
 

bleemeo_agent/core.py
@@ -1302,6 +1302,7 @@ class Core:
             install_thread_hook(self.sentry_client)
 
     def add_scheduled_job(self, func, seconds, args=None, next_run_in=None):
+        # pylint: disable=too-many-branches
         """ Schedule a recuring job using APScheduler
 
             It's a wrapper to add_job/add_interval_job+add_date_job depending
@@ -1350,6 +1351,8 @@ class Core:
                 **options
             )
         else:
+            if next_run_in is not None and next_run_in < 1.0:
+                next_run_in = 1
             if seconds is None or seconds == 0:
                 if next_run_in is None:
                     raise ValueError(
@@ -1365,9 +1368,6 @@ class Core:
                     **options
                 )
             else:
-                if next_run_in is not None and next_run_in < 1.0:
-                    next_run_in = 1
-
                 if next_run_in is not None:
                     options['start_date'] = (
                         datetime.datetime.now() +