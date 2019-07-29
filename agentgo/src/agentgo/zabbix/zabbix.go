7ba5fb75c5388f31c24515bb7c66cad740e88473
Jean Bougerolles <jean.bougerolles@bleemeo.com>
2019-07-29 15:11:21 +0200

    connection handle functions

---
+++ agentgo/src/agentgo/zabbix/zabbix.go
@@ -0,0 +1,115 @@
+package zabbix
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"log"
+)
+
+type packetStruct struct {
+	version int8
+	data    string
+}
+
+type callback func(key string, args []string) string
+
+func handleConnection(c io.ReadWriteCloser, cb callback) {
+	decodedRequest, err := decode(c)
+	if err != nil {
+		log.Printf("Unable to decode Zabbix packet: %v", err)
+		c.Close()
+		return
+	}
+
+	var answer packetStruct
+	answer.data = cb(decodedRequest.data, []string{""})
+	answer.version = decodedRequest.version
+
+	var encodedAnswer []byte
+	if answer.version == 1 {
+		encodedAnswer, err = encodev1(answer)
+	}
+	if err != nil {
+		log.Println(err)
+		c.Close()
+		return
+	}
+
+	_, err = c.Write(encodedAnswer)
+	if err != nil {
+		log.Printf("Answer writing failed: %v", err)
+	}
+
+	c.Close()
+}
+
+func decode(r io.Reader) (packetStruct, error) {
+	packetHead := make([]byte, 13)
+	_, err := r.Read(packetHead)
+	if err != nil {
+		return packetStruct{}, err
+	}
+	var decodedPacket packetStruct
+	var header int32
+	buf := bytes.NewReader(packetHead)
+	err = binary.Read(buf, binary.BigEndian, &header)
+	if err != nil {
+		err = fmt.Errorf("binary.Read failed for packet_version: %v", err)
+		return decodedPacket, err
+	}
+	if header != int32(1514297412) { //1514297412= 0x5a, 0x42, 0x58, 0x44
+		err = fmt.Errorf("wrong packet header")
+		return decodedPacket, err
+	}
+	err = binary.Read(buf, binary.BigEndian, &decodedPacket.version)
+	if err != nil {
+		err = fmt.Errorf("binary.Read failed for packet_version: %v", err)
+		return decodedPacket, err
+	}
+	var dataLength int64
+	err = binary.Read(buf, binary.LittleEndian, &dataLength)
+	if err != nil {
+		err = fmt.Errorf("binary.Read failed for packet_version: %v", err)
+		return decodedPacket, err
+	}
+	packetData := make([]byte, dataLength)
+	_, err = r.Read(packetData)
+	if err != nil {
+		err = fmt.Errorf("r.Read failed for data: %v", err)
+		return decodedPacket, err
+	}
+	decodedPacket.data = string(packetData)
+	return decodedPacket, err
+}
+
+func encodev1(decodedPacket packetStruct) ([]byte, error) {
+	encodedPacket := make([]byte, 13+len(decodedPacket.data))
+
+	copy(encodedPacket[0:4], []byte("ZBXD"))
+
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.BigEndian, &decodedPacket.version)
+	if err != nil {
+		err = fmt.Errorf("binary.Write failed for result_code: %v", err)
+		return encodedPacket, err
+	}
+	copy(encodedPacket[4:5], buf.Bytes())
+
+	var dataLength = int64(len(decodedPacket.data))
+	buf = new(bytes.Buffer)
+	err = binary.Write(buf, binary.LittleEndian, &dataLength)
+	if err != nil {
+		err = fmt.Errorf("binary.Write failed for data_length: %v", err)
+		return encodedPacket, err
+	}
+	copy(encodedPacket[5:13], buf.Bytes())
+
+	copy(encodedPacket[13:13+len(decodedPacket.data)], []byte(decodedPacket.data))
+	return encodedPacket, nil
+}
+
+func run() {
+	return
+}
---
+++ agentgo/src/agentgo/zabbix/zabbix_test.go
@@ -0,0 +1,63 @@
+package zabbix
+
+import (
+	"bytes"
+	"io"
+	"testing"
+)
+
+var versionRequest = []byte{0x5a, 0x42, 0x58, 0x44,
+	0x01, 0x0d, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x61, 0x67, 0x65, 0x6e, 0x74, 0x2e, 0x76, 0x65, 0x72, 0x73, 0x69, 0x6f, 0x6e}
+var pingRequest = []byte{0x5a, 0x42, 0x58, 0x44,
+	0x01, 0x0a, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x61, 0x67, 0x65, 0x6e, 0x74, 0x2e, 0x70, 0x69, 0x6e, 0x67}
+var pingAnswer = []byte{0x5a, 0x42, 0x58, 0x44,
+	0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x31}
+var versionAnswer = []byte{0x5a, 0x42, 0x58, 0x44,
+	0x01, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x34, 0x2e, 0x32, 0x2e, 0x34}
+
+func TestDecode(t *testing.T) {
+	cases := []struct {
+		in   io.Reader
+		want packetStruct
+	}{
+		{bytes.NewReader(versionRequest), packetStruct{1, "agent.version"}},
+		{bytes.NewReader(pingRequest), packetStruct{1, "agent.ping"}},
+	}
+	for _, c := range cases {
+		got, err := decode(c.in)
+		if got != c.want {
+			t.Errorf("decode(zabbixPacket) == %v, want %v", got, c.want)
+		}
+		if err != nil {
+			t.Error(err)
+		}
+	}
+}
+
+func TestEncode(t *testing.T) {
+	cases := []struct {
+		in   packetStruct
+		want []byte
+	}{
+		{packetStruct{1, "1"}, pingAnswer},
+	}
+	for _, c := range cases {
+		got, err := encodev1(c.in)
+		if len(got) != len(c.want) {
+			t.Errorf("encodeV2(%v) == %v, want %v", c.in, got, c.want)
+			break
+		}
+		for i := 0; i < len(got); i++ {
+			if got[i] != c.want[i] {
+				t.Errorf("encodeV2(%v) == %v, want %v", c.in, got, c.want)
+				break
+			}
+		}
+		if err != nil {
+			t.Error(err)
+		}
+	}
+}
