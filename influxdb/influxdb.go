32fea49657e4d928c8ec8ecca31885d3b4282deb
Adrien Gaillard <adrien.gaillard@bleemeo.com>
2019-10-10 11:26:34 +0200

    Add code notifier and metrics notifier for influxDB.

--- influxdb/influxdb.go
+++ influxdb/influxdb.go
@@ -16,3 +16,67 @@
 
 package influxdb
 
+import (
+	bleemeoTypes "glouton/bleemeo/types"
+	"glouton/types"
+
+	"sync"
+)
+
+// Option contians parameters for le influxDB client
+type Option struct {
+	bleemeoTypes.GlobalOption
+}
+
+// Metric point for influxDB
+type InfluxDBPoint struct {
+	// à vérifier si cette struct n'existe pas dans le clien influx db
+}
+
+// Client is an MQTT client for Bleemeo Cloud platform
+type Client struct {
+	option Option
+
+	lock                  sync.Mutex
+	bleemeoPendingPoints  []types.MetricPoint
+	InfluxDBPendingPoints []InfluxDBPoint
+}
+
+// New create a new influxDB client
+func New(option Option) *Client {
+}
+
+// Initialize the parameters to communicate with influxDB server
+func (c *Client) setupInfluxDB() error {
+}
+
+// Connect influxDB client to the server and returns true if the connection is established
+func (c *Client) connect() bool {
+}
+
+// Add metrics points to the the client attribute BleemeopendingPoints
+func (c *Client) addPoints(points []types.MetricPoint) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	c.bleemeoPendingPoints = append(c.bleemeoPendingPoints, points...)
+}
+
+// Convert the BleemeoPendingPoints in InfluxDBPendingPoints
+func (c *Client) convertPendingPoints() {
+}
+
+// Run the influxDB service
+func (c *Client) run() error {
+	// Initialize the server parameters
+	if err := c.setupInfluxDB(); err != nil {
+		return err
+	}
+	// Connect the client to the server
+	c.connect()
+
+	// Suscribe to the Store to receive the metrics
+	storeNotifieeID := c.option.Store.AddNotifiee(c.addPoints)
+
+	// Convert the BleemeoPendingPoints in InfluxDBPendingPoints
+	c.convertPendingPoints()
+}
