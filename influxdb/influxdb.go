b8aac6df84e219e625e7b3c38888e0df783ba2c9
Adrien Gaillard <adrien.gaillard@bleemeo.com>
2019-10-16 15:30:04 +0200

    Add functions to connect glouton to an influxDB server

--- influxdb/influxdb.go
+++ influxdb/influxdb.go
@@ -20,6 +20,8 @@ import (
 	"context"
 	"fmt"
 	"glouton/types"
+	"math"
+	"time"
 
 	"sync"
 
@@ -28,14 +30,14 @@ import (
 
 // Point is the influxBD MetricPoint
 type Point struct {
-	// à vérifier si cette struct n'existe pas dans le clien influx db
+	// à vérifier si cette struct n'existe pas dans le clien influxdb
 }
 
 // Client is an MQTT client for Bleemeo Cloud platform
 type Client struct {
-	serverAddress string
-	dataBaseName  string
-
+	serverAddress         string
+	dataBaseName          string
+	influxClient          influxDBClient.Client
 	lock                  sync.Mutex
 	bleemeoPendingPoints  []types.MetricPoint
 	InfluxDBPendingPoints []Point
@@ -43,21 +45,61 @@ type Client struct {
 
 // New create a new influxDB client
 func New(serverAddress, dataBaseName string) *Client {
-	c, err := influxDBClient.NewHTTPClient(influxDBClient.HTTPConfig{
-		Addr: "http://localhost:8086",
-	})
-	if err != nil {
-		fmt.Println("Error creating InfluxDB Client: ", err.Error())
+	return &Client{
+		serverAddress: serverAddress,
+		dataBaseName:  dataBaseName,
+		influxClient:  nil,
 	}
-	defer c.Close()
-}
-
-// Initialize the parameters to communicate with influxDB server
-func (c *Client) setupInfluxDB() error {
 }
 
 // Connect influxDB client to the server and returns true if the connection is established
-func (c *Client) connect() bool {
+func (c *Client) doConnect() (bool, error) {
+	// Create the influxBD client
+	influxClient, err := influxDBClient.NewHTTPClient(influxDBClient.HTTPConfig{
+		Addr: c.serverAddress,
+	})
+	if err != nil {
+		fmt.Println("Error creating InfluxDB Client: ", err.Error())
+		return false, err
+	}
+	fmt.Println("Connexion influxDB succed")
+	c.influxClient = influxClient
+
+	// Create the database
+	query := influxDBClient.Query{
+		Command: fmt.Sprintf("CREATE DATABASE %s", c.dataBaseName),
+	}
+	response, err := influxClient.Query(query)
+	if err == nil && response.Error() == nil {
+		fmt.Println("Database created: ", response.Results)
+		return true, nil
+	}
+
+	// If the database creation failed we print and return the error
+	if response.Error() != nil {
+		fmt.Println("Error creating InfluxDB DATABASE: ", response.Error())
+		return false, response.Error()
+	}
+	fmt.Println("Error creating InfluxDB DATABASE: ", err.Error())
+	return false, err
+}
+
+// Try to connect the influxDB client to the server and create the database.
+// Retry this operation after a delay if it fails.
+func (c *Client) connect(ctx context.Context) {
+	var sleepDelay time.Duration = 10 * time.Second
+	for ctx.Err() != nil {
+		connectionSucced, _ := c.doConnect()
+		if connectionSucced == true {
+			return
+		}
+		select {
+		case <-ctx.Done():
+			return
+		case <-time.After(sleepDelay * time.Second):
+		}
+		sleepDelay = time.Duration(math.Min(sleepDelay.Seconds()*2, 300)) * time.Second
+	}
 }
 
 // Add metrics points to the the client attribute BleemeopendingPoints
@@ -73,16 +115,14 @@ func (c *Client) convertPendingPoints() {
 
 // Run the influxDB service
 func (c *Client) Run(ctx context.Context) error {
-	// Initialize the server parameters
-	if err := c.setupInfluxDB(); err != nil {
-		return err
-	}
-	// Connect the client to the server
-	c.connect()
+
+	// Connect the client to the server and create the database
+	c.connect(ctx)
 
 	// Suscribe to the Store to receive the metrics
 	storeNotifieeID := c.option.Store.AddNotifiee(c.addPoints)
 
 	// Convert the BleemeoPendingPoints in InfluxDBPendingPoints
 	c.convertPendingPoints()
+	return nil
 }
