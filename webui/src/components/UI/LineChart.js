4ab4a7ec3fc80f04ec990137ab7af9afd9332d4a
tom <tom.darneix@bleemeo.com>
2019-11-13 14:43:34 +0100

    Fix lint error

--- webui/src/components/UI/LineChart.js
+++ webui/src/components/UI/LineChart.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState, useCallback } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { Card } from 'tabler-react'
 import d3 from 'd3'
