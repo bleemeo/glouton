4eb40e5f7f5d0449ab07de3013925866ebc11f3e
Simon Thoby <simon.thoby@bleemeo.com>
2020-07-20 16:48:23 +0200

    logger/windows[file rotation]: do not loose log files when restarting glouton in the same 'period'

--- logger/windows.go
+++ logger/windows.go
@@ -33,28 +33,76 @@ type rotatingLogs struct {
 	lastRotate   time.Time
 }
 
+func (r *rotatingLogs) moveOldFile() error {
+	// rotate the file and sets its name in function of its "end time"
+	// Note: we're not using a classic format like RC3339 because windows doesn't like some characters, for instance ':'
+	filename := r.location + r.filename + ".log"
+	oldFilename := r.location + r.filename + "." + r.lastRotate.Format("2006-01-02T15-04") + ".log"
+
+	oldFile, err := os.OpenFile(oldFilename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0755)
+	if err != nil {
+		return err
+	}
+
+	defer oldFile.Close()
+
+	curFile, err := os.OpenFile(filename, os.O_RDONLY, 0)
+	if err != nil {
+		return err
+	}
+
+	defer curFile.Close()
+
+	var n int
+
+	// copy the file at the end of the previous log file
+	buf := make([]byte, 1<<16)
+
+	for {
+		n, err = curFile.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+			return err
+		}
+
+		cur := 0
+		for cur < n {
+			nOut, err := oldFile.Write(buf[cur:n])
+			if err != nil {
+				return err
+			}
+
+			cur += nOut
+		}
+	}
+
+	return nil
+}
+
 func (r *rotatingLogs) open() error {
 	if r.fd != nil {
 		return nil
 	}
 
+	r.lastRotate = time.Now().Truncate(r.rotatePeriod)
+
 	// this is safe, as calls to the logger are wrapped in a mutex, so no concurrent calls should be made,
 	// and no one wil attempt to write to this logger while "closed"
 	filename := r.location + r.filename + ".log"
 
+	// if the destination already exists, move the current content to another file (rotate it)
 	_, err := os.Stat(filename)
 	if err == nil {
-		// rotate the file and sets its name in function of its "end time"
-		// Note: we're not using a classic format like RC3339 because windows doesn't like som characters, for instance ':'
-		oldFilename := r.location + r.filename + "." + time.Now().Truncate(r.rotatePeriod).Format("2006-01-02T15-04") + ".log"
-
-		err = os.Rename(filename, oldFilename)
+		err = r.moveOldFile()
 		if err != nil {
 			return err
 		}
 	}
 
-	fd, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0755)
+	fd, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0755)
 	if err != nil {
 		return err
 	}
@@ -77,8 +125,6 @@ func (r *rotatingLogs) Write(p []byte) (n int, err error) {
 		if err != nil {
 			return 0, err
 		}
-
-		r.lastRotate = time.Now()
 	}
 
 	return fmt.Fprintf(r.fd, "[%s] %s", time.Now().Format(time.RFC3339), p)
@@ -89,8 +135,8 @@ func (cfg *config) enableSyslog() error {
 		location:     `C:\ProgramData\glouton\logs\`,
 		filename:     "glouton",
 		rotatePeriod: time.Hour,
-		lastRotate:   time.Now(),
 	}
+	writer.lastRotate = time.Now().Truncate(writer.rotatePeriod)
 
 	err := writer.open()
 	if err != nil {
