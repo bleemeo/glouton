897b6569ecad027a8656494ea4d24e0e3afdf6ce
Simon Thoby <simon.thoby@bleemeo.com>
2020-07-21 16:27:40 +0200

    facts: retrieve information from the instance metadata service on Microsoft Azure

--- facts/facts.go
+++ facts/facts.go
@@ -18,6 +18,7 @@ package facts
 
 import (
 	"context"
+	"encoding/json"
 	"glouton/logger"
 	"glouton/version"
 	"io/ioutil"
@@ -179,6 +180,13 @@ func (f *FactProvider) updateFacts(ctx context.Context) {
 		newFacts["virtual"] = guessVirtual(newFacts)
 	}
 
+	// while this could also identify a Surface device, it is much more probable that this is a VM (or maybe a container) on Microsoft Azure
+	if newFacts["system_vendor"] == "Microsoft Corporation" {
+		for k, v := range azureFacts(ctx) {
+			newFacts[k] = v
+		}
+	}
+
 	if strings.Contains(newFacts["bios_version"], "amazon") {
 		for k, v := range awsFacts(ctx) {
 			newFacts[k] = v
@@ -253,6 +261,129 @@ func getFQDN(ctx context.Context) (hostname string, fqdn string) {
 	}
 
 	return
+}
+
+type azureIpAddress struct {
+	Private string `json:"privateIpAddress"`
+	Public  string `json:"publicIpAddress,omitempty"`
+}
+
+type azureIpSubnet struct {
+	IP     string `json:"address"`
+	Prefix string `json:"prefix"`
+}
+
+type azureInterfaceIP4 struct {
+	Addresses []azureIpAddress `json:"ipAddress"`
+	Subnets   []azureIpSubnet  `json:"subnet"`
+}
+
+/*
+// We do not return ipv6 adresses for now
+type azureInterfaceIP6 struct {
+	Addresses []azureIpAddress `json:"ipAddress"`
+	// there is no subnet information for ipv6 adresses, for whatever reason
+}
+*/
+
+type azureNetworkInterface struct {
+	IPv4 azureInterfaceIP4 `json:"ipv4"`
+	//IPv6       azureInterfaceIP6 `json:"ipv6"`
+	MacAddress string `json:"macAddress"`
+}
+
+type azureNetworks struct {
+	Interfaces []azureNetworkInterface `json:"interface"`
+}
+
+// a VM instance
+type azureCompute struct {
+	ID               string `json:"vmId"`
+	VMSize           string `json:"vmSize"`
+	Location         string `json:"location"`
+	Name             string `json:"name"`
+	PlacementGroupId string `json:"placementGroupId"`
+}
+
+type azureInstance struct {
+	Instance azureCompute  `json:"compute"`
+	Network  azureNetworks `json:"network"`
+}
+
+func azureFacts(ctx context.Context) map[string]string {
+	facts := make(map[string]string)
+
+	instance_data := httpQuery(ctx, "http://169.254.169.254/metadata/instance?api-version=2019-11-01", []string{"Metadata:true"})
+	if instance_data == "" {
+		return facts
+	}
+
+	var inst azureInstance
+
+	err := json.Unmarshal([]byte(instance_data), &inst)
+	if err != nil {
+		logger.V(1).Println(instance_data, err)
+		return facts
+	}
+
+	facts["azure_instance_id"] = inst.Instance.ID
+	facts["azure_instance_type"] = inst.Instance.VMSize
+	facts["azure_local_hostname"] = inst.Instance.Name
+	facts["azure_location"] = inst.Instance.Location
+	if inst.Instance.PlacementGroupId != "" {
+		facts["azure_placement_group_id"] = inst.Instance.PlacementGroupId
+	}
+
+	publicIPs := make([]string, 0, 1)
+	privateIPs := make([]string, 0, 1)
+	subnets := make([]string, 0, 1)
+
+	// collect network informations (the list of public ips,private ips and the subnets theses privates IPs belong to)
+	for _, network := range inst.Network.Interfaces {
+		interfaceSubnets := make([]net.IPNet, 0, len(network.IPv4.Subnets))
+		for _, sub := range network.IPv4.Subnets {
+			prefix, err := strconv.Atoi(sub.Prefix)
+			if err != nil {
+				logger.V(2).Printf("facts: couldn't parse azure network subnets: %v", err)
+				return facts
+			}
+
+			interfaceSubnets = append(interfaceSubnets, net.IPNet{IP: net.ParseIP(sub.IP), Mask: net.CIDRMask(prefix, 32)})
+		}
+
+		// At the time I write this, an instance cannot have a public Ipv6 address on Azure (public IPv6 are only available on Azure LBs)
+		for _, ip := range network.IPv4.Addresses {
+			if ip.Public != "" {
+				ip := net.ParseIP(ip.Public)
+				publicIPs = append(publicIPs, ip.String())
+			}
+
+			ip := net.ParseIP(ip.Private)
+			foundSubnet := false
+
+			for _, sub := range interfaceSubnets {
+				if sub.Contains(ip) {
+					foundSubnet = true
+					privateIPs = append(privateIPs, ip.String())
+					subnets = append(subnets, sub.String())
+
+					break
+				}
+			}
+
+			if !foundSubnet {
+				logger.V(2).Printf("facts: couldn't find the network subnet for ip %s", ip)
+			}
+		}
+	}
+
+	if len(publicIPs) > 0 {
+		facts["azure_network_public_ips"] = strings.Join(publicIPs, ",")
+		facts["azure_network_private_ips"] = strings.Join(privateIPs, ",")
+		facts["azure_network_private_subnets"] = strings.Join(subnets, ",")
+	}
+
+	return facts
 }
 
 func awsFacts(ctx context.Context) map[string]string {
@@ -392,9 +523,22 @@ func macAddressByAddress(ctx context.Context, ipAddress string) string {
 }
 
 func urlContent(ctx context.Context, url string) string {
+	return httpQuery(ctx, url, []string{})
+}
+
+func httpQuery(ctx context.Context, url string, headers []string) string {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return ""
+	}
+
+	for _, h := range headers {
+		splits := strings.SplitN(h, ":", 2)
+		if len(splits) != 2 {
+			continue
+		}
+
+		req.Header.Add(splits[0], splits[1])
 	}
 
 	resp, err := http.DefaultClient.Do(req.WithContext(ctx))
