90672643bc1c0aef5746029c8cfac96fcf0cb0ef
Pierre Fersing <pierre.fersing@bleemeo.com>
2020-11-19 10:10:28 +0100

    Allow to build with race detector

--- build.sh
+++ build.sh
@@ -12,12 +12,17 @@ case "$1" in
    "go")
       ONLY_GO=1
       ;;
+   "race")
+      ONLY_GO=1
+      WITH_RACE=1
+      ;;
    "no-js")
       SKIP_JS=1
       ;;
    *)
-      echo "Usage: $0 [go|no-js]"
+      echo "Usage: $0 [go|race|no-js]"
       echo "  go: only compile Go"
+      echo "  race: only compile Go with -race"
       echo "  no-js: skip building JS"
       exit 1
 esac
@@ -38,12 +43,18 @@ fi
 
 GORELEASER_VERSION="v0.147.2"
 
-if [ "${ONLY_GO}" = "1" ]; then
+if [ "${ONLY_GO}" = "1" -a "${WITH_RACE}" = "0" ]; then
    docker run --rm -u $UID:`getent group docker|cut -d: -f 3` -e HOME=/go/pkg -e CGO_ENABLED=0 \
       -v $(pwd):/src -w /src ${GO_MOUNT_CACHE} \
       -v /var/run/docker.sock:/var/run/docker.sock \
       --entrypoint '' \
       goreleaser/goreleaser:${GORELEASER_VERSION} sh -c 'go build .'
+elif [ "${ONLY_GO}" = "1" -a "${WITH_RACE}" = "1" ]; then
+   docker run --rm -u $UID:`getent group docker|cut -d: -f 3` -e HOME=/go/pkg -e CGO_ENABLED=1 \
+      -v $(pwd):/src -w /src ${GO_MOUNT_CACHE} \
+      -v /var/run/docker.sock:/var/run/docker.sock \
+      --entrypoint '' \
+      goreleaser/goreleaser:${GORELEASER_VERSION}-cgo sh -c 'go build -ldflags="-linkmode external -extldflags=-static" -race .'
 else
    docker run --rm -u $UID:`getent group docker|cut -d: -f 3` -e HOME=/go/pkg -e CGO_ENABLED=0 \
       -v $(pwd):/src -w /src ${GO_MOUNT_CACHE} \
