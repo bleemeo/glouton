0484bd0701e5086144a0d13113a081cf39cc9e82
Pierre Fersing <pierre.fersing@bleemeo.com>
2020-12-14 14:48:48 +0100

    Reduce number of MySQL metrics
    Skip counter of read-only commands like "SHOW TABLES" or administrative
    commands (create table, alter system...) which are rarely used.

--- inputs/mysql/mysql.go
+++ inputs/mysql/mysql.go
@@ -115,30 +115,17 @@ func transformMetrics(originalContext internal.GatherContext, currentContext int
 		if strings.HasPrefix(metricName, "commands_") {
 			commands := metricName[len("commands_"):]
 			switch commands {
-			case "admin_commands", "alter_db", "alter_db_upgrade", "alter_event",
-				"alter_function", "alter_instance", "alter_procedure", "alter_server",
-				"alter_table", "alter_tablespace", "alter_user", "analyze", "assign_to_keycache",
-				"begin", "binlog", "call_procedure", "change_db", "change_master", "change_repl_filter",
-				"check", "checksum", "commit", "create_db", "create_event", "create_function",
-				"create_index", "create_procedure", "create_server", "create_table", "create_trigger",
-				"create_udf", "create_user", "create_view", "dealloc_sql", "delete", "delete_multi",
-				"do", "drop_db", "drop_event", "drop_function", "drop_index", "drop_procedure",
-				"drop_server", "drop_table", "drop_trigger", "drop_user", "drop_view", "empty_query",
-				"execute_sql", "explain_other", "flush", "get_diagnostics", "grant", "group_replication_start",
-				"group_replication_stop", "ha_close", "ha_open", "ha_read", "help", "insert", "insert_select",
-				"install_plugin", "kill", "load", "lock_tables", "optimize", "preload_keys", "prepare_sql", "purge",
+			case "analyze", "assign_to_keycache",
+				"begin", "binlog", "call_procedure", "change_master", "change_repl_filter",
+				"check", "checksum", "commit", "dealloc_sql", "delete", "delete_multi",
+				"do", "execute_sql", "flush", "group_replication_start",
+				"group_replication_stop", "ha_close", "ha_open", "ha_read", "insert", "insert_select",
+				"kill", "load", "lock_tables", "optimize", "preload_keys", "prepare_sql", "purge",
 				"purge_before_date", "release_savepoint", "rename_table", "rename_user", "repair", "replace",
-				"replace_select", "reset", "resignal", "revoke", "revoke_all", "rollback", "rollback_to_savepoint",
-				"savepoint", "select", "set_option", "show_binlog_events", "show_binlogs", "show_charsets", "show_collations",
-				"show_create_db", "show_create_event", "show_create_func", "show_create_proc", "show_create_table",
-				"show_create_trigger", "show_create_user", "show_databases", "show_engine_logs", "show_engine_mutex",
-				"show_engine_status", "show_errors", "show_events", "show_fields", "show_function_code", "show_function_status",
-				"show_grants", "show_keys", "show_master_status", "show_open_tables", "show_plugins", "show_privileges",
-				"show_procedure_code", "show_procedure_status", "show_processlist", "show_profile", "show_profiles",
-				"show_relaylog_events", "show_slave_hosts", "show_slave_status", "show_status", "show_storage_engines",
-				"show_table_status", "show_tables", "show_triggers", "show_variables", "show_warnings", "shutdown", "signal",
+				"replace_select", "reset", "resignal", "rollback", "rollback_to_savepoint",
+				"savepoint", "select", "set_option", "signal",
 				"slave_start", "slave_stop", "stmt_close", "stmt_execute", "stmt_fetch", "stmt_prepare", "stmt_reprepare",
-				"stmt_reset", "stmt_send_long_data", "truncate", "uninstall_plugin", "unlock_tables", "update", "update_multi",
+				"stmt_reset", "stmt_send_long_data", "truncate", "unlock_tables", "update", "update_multi",
 				"xa_commit", "xa_end", "xa_prepare", "xa_recover", "xa_rollback", "xa_start":
 				newFields[metricName] = value
 				continue
@@ -149,8 +136,15 @@ func transformMetrics(originalContext internal.GatherContext, currentContext int
 		}
 
 		if strings.HasPrefix(metricName, "handler_") {
-			newFields[metricName] = value
-			continue
+			handler := metricName[len("handler_"):]
+			switch handler {
+			case "commit", "delete", "rollback", "update", "write":
+				newFields[metricName] = value
+				continue
+			default:
+				// ignore all other
+				continue
+			}
 		}
 
 		if strings.HasPrefix(metricName, "threads_") {
