7d76c02166dade0696bf222bcc7a44ff4663ac7e
Noé <noe.heuillet@epitech.eu>
2021-03-29 16:39:44 +0200

    added an option to generate a coverage report and an html report of the tests

--- lint.sh
+++ lint.sh
@@ -2,23 +2,54 @@
 
 set -e
 
-UID=$(id -u)
+USER_UID=$(id -u)
 
 LINTER_VERSION=v1.27
+
+
+case "$1" in
+   "")
+      ;;
+   "coverage")
+      GEN_COVERFILE=1
+      ;;
+    "html")
+      GEN_COVERFILE=1
+      GEN_HTML=1
+      ;;
+   *)
+      echo "Usage: $0 [coverage|html]"
+      echo "  coverage: Generates a coverage report as well as a coverage.out file"
+      echo "  html: Generates a coverage report file then generates an html report"
+      exit 1
+esac
+
 
 if [ -e .build-cache ]; then
    GO_MOUNT_CACHE="-v $(pwd)/.build-cache:/go/pkg"
 fi
 
-docker run --rm -v $(pwd):/app -u $UID ${GO_MOUNT_CACHE} -e HOME=/go/pkg \
-    -w /app golangci/golangci-lint:${LINTER_VERSION} \
-    sh -c 'go test ./... && go test -race ./...'
+if [ "${GEN_COVERFILE}" != "1" ]; then
+    docker run --rm -v "$(pwd)":/app -u "$USER_UID" ${GO_MOUNT_CACHE} -e HOME=/go/pkg \
+        -w /app golangci/golangci-lint:${LINTER_VERSION} \
+        sh -c 'go test ./... && go test -race ./...'
+else
+    docker run --rm -v "$(pwd)":/app -u "$USER_UID" ${GO_MOUNT_CACHE} -e HOME=/go/pkg \
+        -w /app golangci/golangci-lint:${LINTER_VERSION} \
+        sh -c 'go test ./... --coverprofile=coverage.out && go test -race ./...'
+fi
+if [ "${GEN_HTML}" = "1" ]; then
+    docker run --rm -v "$(pwd)":/app -u "$USER_UID" ${GO_MOUNT_CACHE} -e HOME=/go/pkg \
+        -w /app golangci/golangci-lint:${LINTER_VERSION} \
+        sh -c 'go tool cover -html=coverage.out -o coverage.html'
+fi
 
-docker run --rm -v $(pwd):/app -u $UID ${GO_MOUNT_CACHE} -e HOME=/go/pkg \
+
+docker run --rm -v "$(pwd)":/app -u "$USER_UID" ${GO_MOUNT_CACHE} -e HOME=/go/pkg \
     -e GOOS=linux -e GOARCH=amd64 -w /app golangci/golangci-lint:${LINTER_VERSION} \
     golangci-lint run
 
-docker run --rm -v $(pwd):/app -u $UID ${GO_MOUNT_CACHE} -e HOME=/go/pkg \
+docker run --rm -v "$(pwd)":/app -u "$USER_UID" ${GO_MOUNT_CACHE} -e HOME=/go/pkg \
     -e GOOS=windows -e GOARCH=amd64 -w /app golangci/golangci-lint:${LINTER_VERSION} \
     golangci-lint run
 
