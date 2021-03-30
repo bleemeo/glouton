badf8cbe3474fdfcb1058e6574b4e991925e138d
Noé <noe.heuillet@epitech.eu>
2021-03-30 11:03:25 +0200

    Addressed the Merge Request comments

--- lint.sh
+++ lint.sh
@@ -13,14 +13,9 @@ case "$1" in
    "coverage")
       GEN_COVERFILE=1
       ;;
-    "html")
-      GEN_COVERFILE=1
-      GEN_HTML=1
-      ;;
    *)
-      echo "Usage: $0 [coverage|html]"
-      echo "  coverage: Generates a coverage report as well as a coverage.out file"
-      echo "  html: Generates a coverage report file then generates an html report"
+      echo "Usage: $0 [coverage]"
+      echo "  coverage: Generates a coverage report file and generate an html report based on the file"
       exit 1
 esac
 
@@ -33,24 +28,18 @@ if [ "${GEN_COVERFILE}" != "1" ]; then
     docker run --rm -v "$(pwd)":/app -u "$USER_UID" ${GO_MOUNT_CACHE} -e HOME=/go/pkg \
         -w /app golangci/golangci-lint:${LINTER_VERSION} \
         sh -c 'go test ./... && go test -race ./...'
-else
-    docker run --rm -v "$(pwd)":/app -u "$USER_UID" ${GO_MOUNT_CACHE} -e HOME=/go/pkg \
-        -w /app golangci/golangci-lint:${LINTER_VERSION} \
-        sh -c 'go test ./... --coverprofile=coverage.out && go test -race ./...'
-fi
-if [ "${GEN_HTML}" = "1" ]; then
-    docker run --rm -v "$(pwd)":/app -u "$USER_UID" ${GO_MOUNT_CACHE} -e HOME=/go/pkg \
-        -w /app golangci/golangci-lint:${LINTER_VERSION} \
-        sh -c 'go tool cover -html=coverage.out -o coverage.html'
-fi
 
-
-docker run --rm -v "$(pwd)":/app -u "$USER_UID" ${GO_MOUNT_CACHE} -e HOME=/go/pkg \
+    docker run --rm -v "$(pwd)":/app -u "$USER_UID" ${GO_MOUNT_CACHE} -e HOME=/go/pkg \
     -e GOOS=linux -e GOARCH=amd64 -w /app golangci/golangci-lint:${LINTER_VERSION} \
     golangci-lint run
 
-docker run --rm -v "$(pwd)":/app -u "$USER_UID" ${GO_MOUNT_CACHE} -e HOME=/go/pkg \
+    docker run --rm -v "$(pwd)":/app -u "$USER_UID" ${GO_MOUNT_CACHE} -e HOME=/go/pkg \
     -e GOOS=windows -e GOARCH=amd64 -w /app golangci/golangci-lint:${LINTER_VERSION} \
     golangci-lint run
 
+else
+    docker run --rm -v "$(pwd)":/app -u "$USER_UID" ${GO_MOUNT_CACHE} -e HOME=/go/pkg \
+        -w /app golangci/golangci-lint:${LINTER_VERSION} \
+        sh -c 'go test ./... --coverprofile=coverage.out && go test -race ./... && go tool cover -html=coverage.out -o coverage.html'
+fi
 echo "Success"
