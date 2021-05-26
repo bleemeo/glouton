f2be3432c28c75eed6801f97ba557c467f4e37a9
florian gabon <florian.gabon@bleemeo.com>
2021-05-26 14:42:44 +0200

    Fix, order. Free should be on top on the widget

--- webui/src/components/Metric/DefaultDashboardMetrics.js
+++ webui/src/components/Metric/DefaultDashboardMetrics.js
@@ -354,14 +354,10 @@ export const widgetsPrometheusLinux = [
     unit: UNIT_BYTE,
     metrics: [
       {
-        query: "node_memory_MemFree_bytes",
-        color: "#98df8a",
-        legend: "free",
-      },
-      {
-        query: "node_memory_Cached_bytes",
-        color: "#dbdb8d",
-        legend: "cached",
+        query:
+          "node_memory_MemTotal_bytes - node_memory_MemFree_bytes -  node_memory_Cached_bytes - node_memory_Buffers_bytes",
+        color: "#aec7e8",
+        legend: "used",
       },
       {
         query: "node_memory_Buffers_bytes",
@@ -369,10 +365,14 @@ export const widgetsPrometheusLinux = [
         legend: "buffered",
       },
       {
-        query:
-          "node_memory_MemTotal_bytes - node_memory_MemFree_bytes -  node_memory_Cached_bytes - node_memory_Buffers_bytes",
-        color: "#aec7e8",
-        legend: "used",
+        query: "node_memory_Cached_bytes",
+        color: "#dbdb8d",
+        legend: "cached",
+      },
+      {
+        query: "node_memory_MemFree_bytes",
+        color: "#98df8a",
+        legend: "free",
       },
     ],
   },
@@ -548,14 +548,10 @@ export const widgetsPrometheusWindows = [
     unit: UNIT_BYTE,
     metrics: [
       {
-        query: "node_memory_MemFree_bytes",
-        color: "#98df8a",
-        legend: "free",
-      },
-      {
-        query: "node_memory_Cached_bytes",
-        color: "#dbdb8d",
-        legend: "cached",
+        query:
+          "node_memory_MemTotal_bytes - node_memory_MemFree_bytes -  node_memory_Cached_bytes - node_memory_Buffers_bytes",
+        color: "#aec7e8",
+        legend: "used",
       },
       {
         query: "node_memory_Buffers_bytes",
@@ -563,10 +559,14 @@ export const widgetsPrometheusWindows = [
         legend: "buffered",
       },
       {
-        query:
-          "node_memory_MemTotal_bytes - node_memory_MemFree_bytes -  node_memory_Cached_bytes - node_memory_Buffers_bytes",
-        color: "#aec7e8",
-        legend: "used",
+        query: "node_memory_Cached_bytes",
+        color: "#dbdb8d",
+        legend: "cached",
+      },
+      {
+        query: "node_memory_MemFree_bytes",
+        color: "#98df8a",
+        legend: "free",
       },
     ],
   },
