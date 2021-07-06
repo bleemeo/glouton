d7a92cbca577ef7cfd1e83de275fbbac257ddcfc
florian gabon <florian.gabon@bleemeo.com>
2021-07-06 17:19:44 +0200

    Fix, rename a configuration setting

--- agent/agent.go
+++ agent/agent.go
@@ -672,7 +672,7 @@ func (a *agent) run() { //nolint:gocyclo
 
 	if snmpCfg, found := a.config.Get("metric.prometheus.snmp"); found {
 		if configList, ok := snmpCfg.([]interface{}); ok {
-			address, _ := a.config.Get("metric.prometheus.adress.snmp")
+			address, _ := a.config.Get("metric.prometheus.address.snmp")
 			port, _ := a.config.Get("metric.prometheus.port.snmp")
 			snmpTargets = snmp.ConfigToURLs(configList, address.(string), port.(string))
 		}
