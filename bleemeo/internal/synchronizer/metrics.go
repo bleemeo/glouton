32d06013d11e7d069fdc02aea4f735d01948595d
florian gabon <florian.gabon@bleemeo.com>
2021-07-30 17:46:47 +0200

    Update, first to fix the creation of metrics belonging to snmp agent and not the glouton

--- bleemeo/internal/synchronizer/metrics.go
+++ bleemeo/internal/synchronizer/metrics.go
@@ -994,6 +994,18 @@ func (s *Synchronizer) metricRegisterAndUpdateOne(metric types.Metric, registere
 		}
 
 		if !found {
+			agents := s.option.Cache.AgentsByUUID()
+			for _, agent := range agents {
+				if agent.ID == metric.Annotations().BleemeoItem {
+					payload.Agent = agent.ID
+					found = true
+
+					break
+				}
+			}
+		}
+
+		if !found {
 			// This is not an error: either this is a metric from a local monitor, and it should
 			// never be registred, or this is from a monitor that is not yet loaded, and it is
 			// not an issue per se as it will get registered when monitors are loaded, as it will
