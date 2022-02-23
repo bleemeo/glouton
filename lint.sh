4da7e5c16a26436b2a47a273545923e5af431dec
Pierre Fersing <pierre.fersing@bleemeo.com>
2022-02-23 09:43:53 +0100

    Make linter working on MacOS
    When node_modules exists on host, the linter that run inside a Docker is
    too slow to list files inside node_modules.
    Since the linter don't need such files, add a tmpfs to hide files inside node_modules.

--- lint.sh
+++ lint.sh
@@ -16,11 +16,11 @@ docker run --rm -v "$(pwd)":/app ${GO_MOUNT_CACHE} -e HOME=/go/pkg \
    sh -c 'go test ./... --coverprofile=coverage.out -count 1 && go tool cover -html=coverage.out -o coverage.html && go test -race ./... -count 1'
 
 docker run --rm -v "$(pwd)":/app ${GO_MOUNT_CACHE} -e HOME=/go/pkg \
-   -e GOOS=linux -e GOARCH=amd64 -w /app golangci/golangci-lint:${LINTER_VERSION} \
+   -e GOOS=linux -e GOARCH=amd64 --tmpfs /app/webui/node_modules:exec -w /app golangci/golangci-lint:${LINTER_VERSION} \
    golangci-lint run
 
 docker run --rm -v "$(pwd)":/app ${GO_MOUNT_CACHE} -e HOME=/go/pkg \
-   -e GOOS=windows -e GOARCH=amd64 -w /app golangci/golangci-lint:${LINTER_VERSION} \
+   -e GOOS=windows -e GOARCH=amd64 --tmpfs /app/webui/node_modules:exec -w /app golangci/golangci-lint:${LINTER_VERSION} \
    golangci-lint run
 
 echo "Success"
