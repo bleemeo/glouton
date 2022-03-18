e03cc53fc43e096c660f79c8bb3ee2c3a4295c3a
Tom Pillot <tom.pillot@bleemeo.com>
2022-03-18 16:57:23 +0100

    Add process check tests

--- check/process_test.go
+++ check/process_test.go
@@ -12,10 +12,34 @@ type mockProcessProvider struct{}
 
 func (ps mockProcessProvider) Processes(ctx context.Context, maxAge time.Duration) (processes map[int]facts.Process, err error) {
 	procs := map[int]facts.Process{
-		354:  {CmdLine: "", Status: facts.ProcessStatusIOWait},
-		5795: {CmdLine: "postgres -c log_temp_files=0 -c shared_buffers=64MB", Status: facts.ProcessStatusSleeping},
-		5642: {CmdLine: "/usr/lib/firefox/firefox -contentproc -childID 18", Status: facts.ProcessStatusRunning},
-		7568: {CmdLine: "/usr/bin/pulseaudio --daemonize=no --log-target=journal", Status: facts.ProcessStatusZombie},
+		354: {
+			CmdLine: "",
+			Status:  facts.ProcessStatusIOWait,
+		},
+		5795: {
+			CmdLine: "postgres -c log_temp_files=0 -c shared_buffers=64MB",
+			Status:  facts.ProcessStatusSleeping,
+		},
+		5642: {
+			CmdLine: "/usr/lib/firefox/firefox -contentproc -childID 18",
+			Status:  facts.ProcessStatusRunning,
+		},
+		7568: {
+			CmdLine: "/usr/bin/pulseaudio --daemonize=no --log-target=journal",
+			Status:  facts.ProcessStatusZombie,
+		},
+		9565: {
+			CmdLine: "/usr/bin/containerd-shim-runc-v2 -namespace moby -id 8848c0d57022221092f275da20f0127f5a7d3892dcf86765ea8a084e8e72f687 -address /run/containerd/containerd.sock",
+			Status:  facts.ProcessStatusZombie,
+		},
+		9572: {
+			CmdLine: "/usr/bin/containerd-shim-runc-v2 -namespace moby -id 1ce70b670dfaa585eda0e6690328ea091813e46361edd2174bd0b04ece026573 -address /run/containerd/containerd.sock",
+			Status:  facts.ProcessStatusZombie,
+		},
+		10067: {
+			CmdLine: "/usr/bin/containerd-shim-runc-v2 -namespace moby -id 04b809455c35a0ae87f1f8475117bed6425585b9303c3fdb591c197dc2dd7923 -address /run/containerd/containerd.sock",
+			Status:  facts.ProcessStatusIOWait,
+		},
 	}
 
 	return procs, nil
@@ -25,31 +49,56 @@ func Test_processMainCheck(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
+		name           string
 		matchProcess   string
-		expectedStatus types.Status
+		expectedStatus types.StatusDescription
 	}{
 		{
-			matchProcess:   "postgres",
-			expectedStatus: types.StatusOk,
+			name:         "ok-simple",
+			matchProcess: "postgres",
+			expectedStatus: types.StatusDescription{
+				CurrentStatus:     types.StatusOk,
+				StatusDescription: "Process found: postgres -c log_temp_files=0 -c shared_buffers=64MB",
+			},
 		},
 		{
-			matchProcess:   "^/usr/lib/firefox/firefox -contentproc -childID 18$",
-			expectedStatus: types.StatusOk,
+			name:         "ok-regex",
+			matchProcess: "^/usr/lib/firefox/firefox -contentproc -childID 18$",
+			expectedStatus: types.StatusDescription{
+				CurrentStatus:     types.StatusOk,
+				StatusDescription: "Process found: /usr/lib/firefox/firefox -contentproc -childID 18",
+			},
 		},
 		{
-			matchProcess:   "redis",
-			expectedStatus: types.StatusCritical,
+			name:         "ok-with-zombies",
+			matchProcess: `/usr/bin/containerd-shim-runc-v2 -namespace moby -id \w+ -address /run/containerd/containerd.sock`,
+			expectedStatus: types.StatusDescription{
+				CurrentStatus:     types.StatusOk,
+				StatusDescription: "Process found: /usr/bin/containerd-shim-runc-v2 -namespace moby -id 04b809455c35a0ae87f1f8475117bed6425585b9303c3fdb591c197dc2dd7923 -address /run/containerd/containerd.sock",
+			},
 		},
 		{
-			matchProcess:   "pulseaudio .* --log-target=journal",
-			expectedStatus: types.StatusCritical,
+			name:         "critical-not-found",
+			matchProcess: "redis",
+			expectedStatus: types.StatusDescription{
+				CurrentStatus:     types.StatusCritical,
+				StatusDescription: "No process matched",
+			},
+		},
+		{
+			name:         "critical-zombie",
+			matchProcess: "pulseaudio .* --log-target=journal",
+			expectedStatus: types.StatusDescription{
+				CurrentStatus:     types.StatusCritical,
+				StatusDescription: "Process found in zombie state: /usr/bin/pulseaudio --daemonize=no --log-target=journal",
+			},
 		},
 	}
 
 	for _, test := range tests {
 		test := test
 
-		t.Run(test.matchProcess, func(t *testing.T) {
+		t.Run(test.name, func(t *testing.T) {
 			t.Parallel()
 
 			pc, err := NewProcess(test.matchProcess, nil, types.MetricAnnotations{}, nil, mockProcessProvider{})
@@ -57,9 +106,11 @@ func Test_processMainCheck(t *testing.T) {
 				t.Errorf("Failed to create process: %v", err)
 			}
 
-			statusDesc := pc.processMainCheck(context.Background())
-			if statusDesc.CurrentStatus != test.expectedStatus {
-				t.Errorf("Expected status %v, got %v", test.expectedStatus, statusDesc.CurrentStatus)
+			status := pc.processMainCheck(context.Background())
+			if status.CurrentStatus != test.expectedStatus.CurrentStatus {
+				t.Errorf("Expected status %v, got %v", test.expectedStatus.CurrentStatus, status.CurrentStatus)
+			} else if status.StatusDescription != test.expectedStatus.StatusDescription {
+				t.Errorf("Expected status description %v, got %v", test.expectedStatus.StatusDescription, status.StatusDescription)
 			}
 		})
 	}
