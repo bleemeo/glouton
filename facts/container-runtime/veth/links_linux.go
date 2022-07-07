5229ac2dcd11478fa28ac1904fd59ca0c94533b2
Tom Pillot <tom.pillot@bleemeo.com>
2022-07-07 10:49:30 +0200

    Don't ignore minikube interface eth0 even if link-netnsid is set

--- facts/container-runtime/veth/links_linux.go
+++ facts/container-runtime/veth/links_linux.go
@@ -13,10 +13,17 @@ func linkList() ([]link, error) {
 	for i, iface := range interfaces {
 		// NetNsID identifies the namespace holding the link, it is only set when the
 		// interface is associated with another network namespace (-1 is the default value).
+		hasNSPeer := iface.Attrs().NetNsID >= 0
+
+		// Don't ignore minikube interface eth0.
+		if hasNSPeer && iface.Attrs().Name == "eth0" {
+			hasNSPeer = false
+		}
+
 		links[i] = link{
 			name:      iface.Attrs().Name,
 			index:     iface.Attrs().Index,
-			hasNSPeer: iface.Attrs().NetNsID >= 0,
+			hasNSPeer: hasNSPeer,
 		}
 	}
 
