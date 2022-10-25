ed851193e9498fc35f337791a185aac85da052c6
Tom Pillot <tom.pillot@bleemeo.com>
2022-10-25 16:13:35 +0200

    Update copyright

--- prometheus/exporter/windows/collector.go
+++ prometheus/exporter/windows/collector.go
@@ -1,3 +1,19 @@
+// Copyright 2015-2022 Bleemeo
+//
+// bleemeo.com an infrastructure monitoring solution in the Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // This code comes from https://github.com/prometheus-community/windows_exporter/blob/f1384759cb04f8a66cd0954e2eff4bca81b2caa4/exporter.go.
 // To see its license, please refer to https://github.com/prometheus-community/windows_exporter/blob/master/LICENSE.
 // The only modification is switching the (deprecated) log provider from prometheus/common/log (backed by
