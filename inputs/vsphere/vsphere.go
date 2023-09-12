45d8b6d971a094100376fa9f91ca85d36b56df07
Thomas Delbende <thomas.delbende@bleemeo.com>
2023-09-12 11:34:31 +0200

    Standardize vSphere metrics

--- inputs/vsphere/vsphere.go
+++ inputs/vsphere/vsphere.go
@@ -46,14 +46,13 @@ func New(url string, username, password string, insecureSkipVerify, monitorVMs b
 	vsphereInput.VMMetricInclude = []string{
 		"cpu.usage.average",
 		"cpu.latency.average",
+		"mem.active.average",
 		"mem.usage.average",
 		"mem.swapped.average",
 		"disk.read.average",
 		"disk.write.average",
-		"disk.usage.average",
 		"net.transmitted.average",
 		"net.received.average",
-		"net.usage.average",
 	}
 	vsphereInput.HostMetricInclude = []string{
 		"cpu.usage.average",
@@ -63,11 +62,10 @@ func New(url string, username, password string, insecureSkipVerify, monitorVMs b
 		"mem.swapout.average",
 		"disk.read.average",
 		"disk.write.average",
-		"disk.usage.average",
 		"net.transmitted.average",
 		"net.received.average",
-		"net.usage.average",
 	}
+	vsphereInput.DatacenterExclude = []string{"*"}
 	vsphereInput.ResourcePoolMetricExclude = []string{"*"}
 	vsphereInput.ClusterMetricExclude = []string{"*"}
 	vsphereInput.DatastoreMetricExclude = []string{"*"}
@@ -90,7 +88,45 @@ func renameMetrics(currentContext internal.GatherContext, metricName string) (ne
 	newMeasurement = currentContext.Measurement
 	newMetricName = strings.TrimSuffix(metricName, "_average")
 
-	return
+	switch newMeasurement {
+	// VM metrics
+	case "vsphere_vm_cpu":
+		newMetricName = strings.Replace(newMetricName, "usage", "used", 1)
+	case "vsphere_vm_mem":
+		if newMetricName == "swapped" {
+			newMeasurement = "vsphere_vm_swap"
+			newMetricName = "used"
+		} else {
+			newMetricName = strings.Replace(newMetricName, "usage", "used", 1)
+		}
+	case "vsphere_vm_disk":
+		newMeasurement = "vsphere_vm_io"
+		newMetricName = strings.Replace(newMetricName, "read", "read_bytes", 1)
+		newMetricName = strings.Replace(newMetricName, "write", "write_bytes", 1)
+	case "vsphere_vm_net":
+		newMetricName = strings.Replace(newMetricName, "received", "bits_recv", 1)
+		newMetricName = strings.Replace(newMetricName, "transmitted", "bits_sent", 1)
+	// Host metrics
+	case "vsphere_host_cpu":
+		newMetricName = strings.Replace(newMetricName, "usage", "used", 1)
+	case "vsphere_host_mem":
+		if newMetricName == "swapout" {
+			newMeasurement = "vsphere_host_swap"
+			newMetricName = "out"
+		} else {
+			newMetricName = strings.Replace(newMetricName, "totalCapacity", "total", 1)
+			newMetricName = strings.Replace(newMetricName, "usage", "used", 1)
+		}
+	case "vsphere_host_disk":
+		newMeasurement = "vsphere_host_io"
+		newMetricName = strings.Replace(newMetricName, "read", "read_bytes", 1)
+		newMetricName = strings.Replace(newMetricName, "write", "write_bytes", 1)
+	case "vsphere_host_net":
+		newMetricName = strings.Replace(newMetricName, "received", "bits_recv", 1)
+		newMetricName = strings.Replace(newMetricName, "transmitted", "bits_sent", 1)
+	}
+
+	return newMeasurement, newMetricName
 }
 
 func transformMetrics(currentContext internal.GatherContext, fields map[string]float64, originalFields map[string]interface{}) map[string]float64 {
@@ -100,17 +136,16 @@ func transformMetrics(currentContext internal.GatherContext, fields map[string]f
 	factors := map[string]map[string]float64{
 		// VM metrics
 		"vsphere_vm_mem": {
+			"active_average":  1000, // KB to B
 			"swapped_average": 1000, // KB to B
 		},
 		"vsphere_vm_disk": {
 			"read_average":  1000, // KB/s to B/s
 			"write_average": 1000, // KB/s to B/s
-			"usage_average": 1000, // KB/s to B/s
 		},
 		"vsphere_vm_net": {
-			"transmitted_average": 1000, // KB/s to B/s
-			"received_average":    1000, // KB/s to B/s
-			"usage_average":       1000, // KB/s to B/s
+			"received_average":    8000, // KB/s to b/s
+			"transmitted_average": 8000, // KB/s to b/s
 		},
 		// Host metrics
 		"vsphere_host_mem": {
@@ -121,12 +156,10 @@ func transformMetrics(currentContext internal.GatherContext, fields map[string]f
 		"vsphere_host_disk": {
 			"read_average":  1000, // KB/s to B/s
 			"write_average": 1000, // KB/s to B/s
-			"usage_average": 1000, // KB/s to B/s
 		},
 		"vsphere_host_net": {
-			"net_transmitted_average": 1000, // KB/s to B/s
-			"net_received_average":    1000, // KB/s to B/s
-			"net_usage_average":       1000, // KB/s to B/s
+			"received_average":    8000, // KB/s to b/s
+			"transmitted_average": 8000, // KB/s to b/s
 		},
 	}
 
