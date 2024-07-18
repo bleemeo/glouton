3d2db831e5dd8c9b2deb5fe0f34d4f7148cab0ac
Thomas Delbende <thomas.delbende@bleemeo.com>
2024-07-18 14:49:18 +0200

    Simplify sensitive container env var detection

--- facts/container-runtime/internal/obfuscation/sensitivity.go
+++ facts/container-runtime/internal/obfuscation/sensitivity.go
@@ -16,26 +16,11 @@ func isSensitive(key string) bool {
 	}
 
 	for potentiallySensitive, unless := range sensitiveKeywords {
-	SensitivityCheck:
-		if sensitiveIdx := strings.Index(key, potentiallySensitive); sensitiveIdx >= 0 {
-			for _, nonSensitive := range unless {
-				nonSensitiveIdx := strings.Index(key, nonSensitive)
-				if nonSensitiveIdx >= 0 {
-					// Ensuring that the non-sensitive part is actually the same part as the sensitive one,
-					// not to consider the key as non-sensitive when it contains
-					// both and distinct sensitive and non-sensitives keywords.
-					posDelta := nonSensitiveIdx - sensitiveIdx
-					if nonSensitiveIdx <= sensitiveIdx && posDelta < len(nonSensitive) {
-						// In fact, this part isn't that much sensitive.
-						// We remove it not to handle it again.
-						key = key[:nonSensitiveIdx] + key[nonSensitiveIdx+len(nonSensitive):]
+		for _, nonSensitive := range unless {
+			key = strings.ReplaceAll(key, nonSensitive, "")
+		}
 
-						// Is the key still potentially sensitive ?
-						goto SensitivityCheck
-					}
-				}
-			}
-
+		if strings.Contains(key, potentiallySensitive) {
 			return true
 		}
 	}
