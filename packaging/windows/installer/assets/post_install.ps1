ba37810ee9db634657976033495d9f90dc45b632
emile <emile.le-gallic@bleemeo.com>
2024-07-25 11:37:38 +0200

    Optional auto update at install

--- packaging/windows/installer/assets/post_install.ps1
+++ packaging/windows/installer/assets/post_install.ps1
@@ -1,37 +1,20 @@
 # This script is executed with admin privileges after installing Glouton.
 param([string]$InstallFolder)
 
-
-# Define the paths and tasks names
+# Define the path and task name
 $WindowsUpdateCheckerScriptPath = "$InstallFolder\windows_update_checker.ps1"
 $TaskWindowsUpdateCheckerName = "Bleemeo\Glouton\Windows Update Checker"
 $WindowsUpdateCheckerScriptLaucnh = "powershell.exe -ExecutionPolicy Bypass -NonInteractive -File `"$WindowsUpdateCheckerScriptPath`""
 
-$AutoUpdateScriptPath = "$InstallFolder\glouton_auto_update.ps1"
-$TaskAutoUpdateName = "Bleemeo\Glouton\Auto Update Task"
-$AutoUpdateScriptLaunch = "powershell.exe -ExecutionPolicy Bypass -NonInteractive -File `"$AutoUpdateScriptPath`""
-
-# Check if the tasks already exists
+# Check if the task already exists
 $taskWindowsUpdateCheckerExists = schtasks /Query /TN $TaskWindowsUpdateCheckerName 2>$null
 if ($?) {
     # If the task exists, delete it
     schtasks /Delete /F /TN $TaskWindowsUpdateCheckerName
 }
 
-$taskAutoUpdateExists = schtasks /Query /TN $TaskAutoUpdateName 2>$null
-if ($?) {
-    # If the task exists, delete it
-    schtasks /Delete /F /TN $TaskAutoUpdateName
-}
-
-# Calculate the start time for the Auto Update task to be 24 hours from now
-$startTime = (Get-Date).AddDays(1).ToString("HH:mm")
-
-# Create the scheduled tasks
+# Create the task and run it
 schtasks /Create /F /RU System /SC HOURLY /TN $TaskWindowsUpdateCheckerName /TR $WindowsUpdateCheckerScriptLaucnh
-schtasks /Create /F /RU System /SC DAILY /ST $startTime /TN $TaskAutoUpdateName /TR $AutoUpdateScriptLaunch
-
-# Run the Windows Update Checker task immediately
 schtasks /Run /I /TN $TaskWindowsUpdateCheckerName
 
 # Make sure all files created in the directory can be read by Glouton.
