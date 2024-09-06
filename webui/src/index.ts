a95cfc8b99c577c8606614723127dbe2651edc9b
Thomas Delbende <thomas.delbende@bleemeo.com>
2024-09-06 16:32:01 +0200

    Prettify JS code

--- webui/src/index.ts
+++ webui/src/index.ts
@@ -1,25 +1,23 @@
-export {}
+export {};
 
-declare let __webpack_public_path__: string;
+declare let __webpack_public_path__: string; //eslint-disable-line
 
 declare global {
   interface Window {
     GloutonConfig: {
-      STATIC_CDN_URL: string,
+      STATIC_CDN_URL: string;
     };
-    __webpack_public_path__: string,
+    __webpack_public_path__: string;
     GloutonPanel: GloutonPanelApp | undefined;
   }
 }
 class GloutonPanelApp {
   start() {
-    /* eslint-disable */
     __webpack_public_path__ =
       process.env.NODE_ENV === "production"
         ? window.GloutonConfig.STATIC_CDN_URL
         : "http://localhost:3015/";
     import("./app");
-    /* eslint-enable */
   }
 }
 
