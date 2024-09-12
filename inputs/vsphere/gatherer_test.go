a041b2a8c49c61928ade34b1536bf8793cd1cfa8
Thomas Delbende <thomas.delbende@bleemeo.com>
2024-09-12 11:11:11 +0200

    Skip vSphere gathering tests cleverly
    Detailed explanation of why they sometime fail:
    - the vSphere simulator has hardcoded points for each metrics of each device type
    - the VM's virtualDisk.read.average metric is the only we use that contains several 0 in a row - see https://github.com/vmware/govmomi/blob/139b19e3641b61b0969f78c84266bfc3e12f109c/simulator/vpx/performance_manager_data.go#L36
    - the XML serializer omits types zero-values, hence 0 for integers
    - so when telegraf asks for 4 points, and they are all 0, we end up with no points at all and the io_read_bytes series (virtualDisk.read.average) is not sent by telegraf, so it is considered missing by the test, which fails.
    
    Since we can "predict" when the simulator will return four zeroes by doing the same calculation done in https://github.com/vmware/govmomi/blob/main/simulator/performance_manager.go#L225 , I tried to do the gathering for timestamps that will not return four zeroes. However, it is telegraf who sets the request timestamp, and we can't do anything about it.
    
    Instead of skipping the test when feeling it will fail, we might also have manually added the missing series to fill its gap, but I don't really like the idea of modifying the result before testing it.
    
    We could also have done some modifications in the simulator's code:
    - to avoid having four zeroes in a row
    - to be able to inject our own CurrentTime
    or in telegraf's code:
    - to decide by ourselves which timestamp we want
    - to inject our own http.Transport layer to rearrange some transactions between telegraf and the simulator

--- inputs/vsphere/gatherer_test.go
+++ inputs/vsphere/gatherer_test.go
@@ -57,7 +57,13 @@ func setupGathering(t *testing.T, dirName string) (mfs []*dto.MetricFamily, defe
 
 	manager.Devices(ctx, 0)
 
-	t0 := time.Now().Truncate(time.Minute)
+	t0 := time.Now().Round(time.Second)
+
+	if willFail, offset := willGatheringTestFail(t0); willFail {
+		t.Skipf("This test is likely to fail (offset=%d)", offset)
+
+		return nil, nil
+	}
 
 	realtimeMfs, err := vSphere.realtimeGatherer.GatherWithState(ctx, registry.GatherState{T0: t0, FromScrapeLoop: true})
 	if err != nil {
@@ -76,10 +82,30 @@ func setupGathering(t *testing.T, dirName string) (mfs []*dto.MetricFamily, defe
 	return mfs, deferFn
 }
 
-//nolint:nolintlint,gofmt, dupl
-func TestGatheringESXI(t *testing.T) { //nolint:maintidx
-	t.Skip("this test is unstable skip until it's fixed")
+// willGatheringTestFail returns whether gathering tests are likely to fail at the given timestamp.
+func willGatheringTestFail(t time.Time) (bool, int) {
+	// fourZeroesOffsets contains the list of all the offset that will make
+	// the simulator return 4 zeroes, which will lead to no series at all.
+	// The third line contains offsets that shouldn't fail,
+	// but sometime do due to a 20s jitter.
+	fourZeroesOffsets := map[int]struct{}{
+		0: {}, 1: {}, 2: {}, 3: {}, 4: {}, 20: {}, 27: {},
+		28: {}, 29: {}, 50: {}, 51: {}, 65: {}, 66: {}, 99: {},
+		5: {}, 21: {}, 30: {}, 52: {}, 67: {},
+	}
 
+	// The offset is evaluated like so:
+	// (start_TS / interval) % simulator_points_count
+	// For full details, see the code around
+	// https://github.com/vmware/govmomi/blob/139b19e3641b61b0969f78c84266bfc3e12f109c/simulator/performance_manager.go#L225
+	offset := int((t.Add(-1*time.Minute).Unix() / 20) % 100)
+	_, found := fourZeroesOffsets[offset]
+
+	return found, offset
+}
+
+//nolint:nolintlint,gofmt,dupl
+func TestGatheringESXI(t *testing.T) { //nolint:maintidx
 	mfs, deferFn := setupGathering(t, "esxi_1")
 	defer deferFn()
 
@@ -481,13 +507,14 @@ func TestGatheringESXI(t *testing.T) { //nolint:maintidx
 
 	if diff := cmp.Diff(expectedMfs, mfs, opts, makeVirtualDiskMetricComparer(opts)); diff != "" {
 		t.Errorf("Unexpected metric families (-want +got):\n%s", diff)
+		// In case it still fails, we want to know for which offset it did so.
+		ts := time.UnixMilli(mfs[0].GetMetric()[0].GetTimestampMs())
+		t.Logf("Ts: %s / Offset: %d", ts, ((ts.Unix())/20)%100)
 	}
 }
 
-//nolint:nolintlint,gofmt, dupl
+//nolint:nolintlint,gofmt,dupl
 func TestGatheringVcsim(t *testing.T) { //nolint:maintidx
-	t.Skip("this test is unstable skip until it's fixed")
-
 	mfs, deferFn := setupGathering(t, "vcenter_1")
 	defer deferFn()
 
@@ -1008,6 +1035,9 @@ func TestGatheringVcsim(t *testing.T) { //nolint:maintidx
 
 	if diff := cmp.Diff(expectedMfs, mfs, opts, makeVirtualDiskMetricComparer(opts)); diff != "" {
 		t.Errorf("Unexpected metric families (-want +got):\n%s", diff)
+		// In case it still fails, we want to know for which offset it did so.
+		ts := time.UnixMilli(mfs[0].GetMetric()[0].GetTimestampMs())
+		t.Logf("Ts: %s / Offset: %d", ts, ((ts.Unix())/20)%100)
 	}
 }
 
