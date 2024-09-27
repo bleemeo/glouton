fba7dba7e69d7e2325cc23c8f334870e111106fa
Pierre Fersing <pierre.fersing@bleemeo.com>
2024-09-27 13:20:50 +0000

    Add tests cases for smarctl error handling

--- inputs/smart/run_cmd_test.go
+++ inputs/smart/run_cmd_test.go
@@ -323,6 +323,16 @@ func TestIsExitCode1(t *testing.T) {
 			expectExitErr: true,
 			expectCode1:   false,
 		},
+  		{
+			err:           testExitError(1),
+			expectExitErr: true,
+			expectCode1:   true,
+		},
+        {
+			err:           testExitError(8),
+			expectExitErr: true,
+			expectCode1:   false,
+		},
 	}
 
 	for i, tc := range testCases {
