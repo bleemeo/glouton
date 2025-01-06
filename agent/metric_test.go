cc8f95b1be99369e903290ee4c61030aa8cf43e7
Thomas Delbende <thomas.delbende@bleemeo.com>
2025-01-06 14:09:15 +0100

    Update & please linters

--- agent/metric_test.go
+++ agent/metric_test.go
@@ -1670,7 +1670,7 @@ func Benchmark_MultipleFilters(b *testing.B) {
 	b.ResetTimer()
 
 	b.Run("or", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
+		for range b.N {
 			var allowed int
 
 			for _, lbls := range metrics {
@@ -1682,7 +1682,7 @@ func Benchmark_MultipleFilters(b *testing.B) {
 	})
 
 	b.Run("merge", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
+		for range b.N {
 			var allowed int
 
 			for _, lbls := range metrics {
